482cccebad06a74f2cfb2618b63c52965658ea13
valotr0n <beloshnez98@bk.ru>
2026-02-19 19:15:35 +0300

    Task7 + almost done Task4 and Task6

--- src/First/VideoPlayerApp.java
+++ src/First/VideoPlayerApp.java
@@ -1,76 +1,129 @@
 package First;
 
 import java.io.File;
-import javax.imageio.ImageIO;
-import javax.swing.text.View;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.*;
-import javafx.scene.image.WritableImage;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
 import javafx.stage.*;
 import javafx.scene.media.*;
+import javafx.util.Duration;
 
 public class VideoPlayerApp extends Application{
-    private BorderPane root;
-    private HBox mediaBarHbox;
+
+    private MediaPlayer mediaPlayer;
+    private MediaView mediaView;
+    private Button playerButton;
+    private Slider time;
+    private Slider volume; 
+    private Label fileNameLabel;
+    private Label totalTimeLabel;
+    private Label currentTimeLabel;
 
     @Override
     public void start(Stage window) {
-        root = new BorderPane();
+        BorderPane root = new BorderPane();
 
-        Menu menuFile = new Menu();
-        MenuItem saveItem = new MenuItem("Сохранить");
-        MenuItem exitItem = new MenuItem("Выйти");
-        menuFile.getItems().addAll(saveItem, exitItem);
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(menuFile);
+        Menu file = new Menu("Файл");
+        MenuItem open = new MenuItem("Открыть");
+        MenuBar menu = new MenuBar();
+        file.getItems().add(open);
+        menu.getMenus().add(file);
+        root.setTop(menu);
 
-        String videoPath = "C:/videos/test.mp4";
-        Media media = new Media(new File(videoPath).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
-        MediaView mediaView = new MediaView(mediaPlayer);
-        mediaPlayer.play();
+        fileNameLabel = new Label();
+        totalTimeLabel = new Label("00:00");
+        currentTimeLabel = new Label("00:00");
+        mediaView = new MediaView();
+        StackPane centerPane = new StackPane(mediaView,fileNameLabel);
+        centerPane.setAlignment(fileNameLabel, Pos.TOP_CENTER);
+        root.setCenter(centerPane);
+        mediaView.fitWidthProperty().bind(centerPane.widthProperty());
+        mediaView.fitHeightProperty().bind(centerPane.heightProperty());
 
-        mediaBarHbox = new HBox();
-        mediaBarHbox.setSpacing(10);
-        mediaBarHbox.setAlignment(Pos.CENTER);
-        Button startButton = new Button("||");
-        Slider time = new Slider();
-        Slider vol = new Slider();
-        vol.setMin(0);
-        vol.setMax(1);
-        vol.setValue(0.5);
-        Label volumeLabel = new Label("Volume: ");
-        mediaBarHbox.getChildren().addAll(startButton,time,volumeLabel,vol);
-        startButton.setOnAction(event -> {
-            if (mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+        open.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Video Files", "*.mp4"));
+            File selectedFile = fileChooser.showOpenDialog(window);
+            if (selectedFile != null) {
+                openVideo(selectedFile);
+            }
+            fileNameLabel.setText(selectedFile.getName());
+        });
+
+
+        playerButton = new Button("||");
+        time = new Slider();
+        volume = new Slider();
+        volume.setMin(0);
+        volume.setMax(1);
+        volume.setValue(0.5);
+        volume.setPrefWidth(120);
+        time.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(time, Priority.ALWAYS);
+        HBox bottomBar = new HBox(10,playerButton,currentTimeLabel,time,totalTimeLabel,volume);
+        bottomBar.setPadding(new Insets(8));
+        time.setMaxWidth(Double.MAX_VALUE);
+        root.setBottom(bottomBar);
+
+
+        playerButton.setOnAction(event -> {
+            MediaPlayer.Status status = mediaPlayer.getStatus();
+            if (status == MediaPlayer.Status.PLAYING) {
                 mediaPlayer.pause();
-                startButton.setText(">");
+                playerButton.setText(">");
             }
             else {
                 mediaPlayer.play();
-                startButton.setText("||");
+                playerButton.setText("||");
             }
         });
 
-        root.setBottom(mediaBarHbox);
-        root.setCenter(mediaView);
-        root.setTop(menuBar);
-
         Scene view = new Scene(root,800,600);
-        window.setTitle("Media Player");
         window.setScene(view);
+        window.setTitle("Video Player");
         window.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    private void openVideo(File file) {
+        Media media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.volumeProperty().bind(volume.valueProperty());
+        mediaPlayer.play();
+
+        mediaPlayer.setOnReady(() -> {
+            time.setMax(mediaPlayer.getTotalDuration().toSeconds());
+            totalTimeLabel.setText(formatTime(mediaPlayer.getTotalDuration()));
+        });
+
+        mediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
+            if (!time.isValueChanging()) {
+                time.setValue(newTime.toSeconds());
+            }
+            currentTimeLabel.setText(formatTime(newTime));
+        });
+
+        time.valueChangingProperty().addListener((obs,wasChanging,isChanging) -> {
+            if(!isChanging) {
+                mediaPlayer.seek(Duration.seconds(time.getValue()));
+            }
+        });
+
+        time.setOnMouseReleased(event -> {
+            mediaPlayer.seek(Duration.seconds(time.getValue()));
+        });
     }
 
+    private String formatTime(Duration duration) {
+        int minutes = (int) duration.toMinutes();
+        int seconds = (int) duration.toSeconds() % 60;
+        return String.format("%02d:%02d", minutes,seconds);
+    }
+    public static void main(String[] args) {
+        launch(VideoPlayerApp.class);
+    }
 }
